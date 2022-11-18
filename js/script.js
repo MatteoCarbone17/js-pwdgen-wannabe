53fb6fa998cc8c497ac8947d898b13d0065e2917
MatteoCarbone17 <matteocarbone17@live.com>
2022-11-18 16:03:05 +0100

    add another prompt

--- js/script.js
+++ js/script.js
@@ -12,5 +12,23 @@ let colorePreferitoUtente = prompt('Qual è il tuo colore preferito ?');
 console.log (colorePreferitoUtente);
 
 
+
 document.getElementById('codicesegreto') .innerHTML =`il tuo codice segreto per accedere a dati ancor piu segreti del codice \' segreto \' è : ${nomeCompleto}${colorePreferitoUtente}22` ;
+
+
+
+
+let year ;
+year = 2022 ;
+
+
+let ageUser = prompt("in che anno sei nato?") ;
+console.log (ageUser);
+
+completeAge = year - ageUser ;
+console.log (completeAge);
+
+
+document.getElementById('codicemoltosegreto') .innerHTML =`il tuo codice segreto per accedere a dati ancor piu segreti del codice \' segreto \' è : ${completeAge}${nomeCompleto}22${colorePreferitoUtente}` ;
+
 
